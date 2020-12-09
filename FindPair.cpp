7c855174e7e9d9e4e287a19ddc1fc46cc42f5e68
toktor <ak.toktor@gmail.com>
2020-12-09 19:12:32 +0200

    I implemented logic

--- FindPair.cpp
+++ FindPair.cpp
@@ -1,8 +1,24 @@
-// FindPair.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <vector>
 #include <iostream>
 
-int main()
-{
+int main() {
+	std::vector<int> myVec = { 7, 9, 1, 7, 8, 0, 9, 1, 8 };
+	int counter = 0;
+	for (std::vector<int>::iterator iterX = myVec.begin(); iterX != myVec.end(); ++iterX) {
+		for (std::vector<int>::iterator iterY = myVec.begin(); iterY != myVec.end(); ++iterY) {
+			if (*(iterX) == *(iterY)) {
+				++counter;
+			}
+			if (counter == 2) {
+				std::cout << "The number has pair: " << *(iterX) << std::endl;
+				break;
+			}
+		}
+		if (counter == 1) {
+			std::cout << "The number doesn't has pair: " << *(iterX) << std::endl;
+		}
+		counter = 0;
+	}
+
+	return 0;
 }
